fbef86d2364a349e716b52a3b39cf020414888fd
zhaolei-1967 <zhaolei_67@sina.com.cn>
2018-06-12 14:51:15 +0800

    添加权限控制

---
+++ demo/src/main/java/me/zhaolei/framework/demo/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package me.zhaolei.framework.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/index").permitAll()
+                .antMatchers("/private/**").hasRole("USER")
+                .and()
+                .formLogin().loginPage("/login").successForwardUrl("/private/index").failureUrl("/login-error");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER"));
+    }
+}
--- demo/src/main/java/me/zhaolei/framework/demo/controller/HomePageControl.java
+++ demo/src/main/java/me/zhaolei/framework/demo/controller/HomePageControl.java
@@ -3,19 +3,48 @@ package me.zhaolei.framework.demo.controller;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class HomePageControl {
-	@GetMapping
-	public ModelAndView list() {
+	@RequestMapping
+	public ModelAndView home() {
+		return new ModelAndView("/index");
+	}
+
+	@RequestMapping("/private/index")
+	public ModelAndView list(Model model) {
+		model.addAttribute("title", "机密页面");
 		ArrayList<String> messages = new ArrayList<String>();
 		messages.add("11111111111");
 		messages.add("22222222");
 		messages.add("3333333333");
-		return new ModelAndView("index", "messages", messages);
+		model.addAttribute("messages", messages);
+		return new ModelAndView("/private/index");
+	}
+
+	/**
+	 * 登录页面。
+	 * @return
+	 */
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	/**
+	 *  登录错误，添加错误标志后跳转回登录页面。
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/login-error")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login";
 	}
 }
